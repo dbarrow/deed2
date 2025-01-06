dfe2e15c58b1be83f763a90e47333d12eef7e6c1
Darryl Barrow <darryl@traversepoint.com>
2025-01-06 07:59:33 -0500

    Add files via upload

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    deps: {
+      inline: [/^vue$/, /^leaflet$/]
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+        '**/*.test.ts',
+        '**/*.config.ts',
+        'src/vite-env.d.ts'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
